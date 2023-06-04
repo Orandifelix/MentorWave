f63abcd557398cc3ab5dcff19bee5b1de27659a4
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-06-04 14:06:54 +0300

    Starting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./components/home/NavBar";
 
 function App() {
   return (
     <div>
-      <h1>Hello from MentorWave</h1>
+      <NavBar />
     </div>
   );
 }
