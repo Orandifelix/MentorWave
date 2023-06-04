b99d46d09d0f5e7b9c60389ea1d59652a0381f51
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-06-04 14:10:13 +0300

    Made Navigation

--- src/components/home/NavBar.jsx
+++ src/components/home/NavBar.jsx
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Menu, Segment, Button } from "semantic-ui-react";
+import "./Navbar.css";
 
-const NavBar = () => {
+const Navbar = () => {
+  const [activeItem, setActiveItem] = useState("Home");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
+
   return (
-    <div className="ui padded menu">
-      <h2 className="item">Home</h2>
-      <h2 className="item">FindMentor</h2>
-      <h2 className="item">About us</h2>
-      <h2 className="item">Contacts</h2>
-      <div className="right menu item"> <button className="ui primary button">Sign In</button></div>
-    </div>
+    <Segment inverted>
+      <Menu inverted pointing secondary>
+        <div className="navbar-logo">
+          {/* <img src="mentorwave-logo.png" alt="MentorWave" /> */}
+          <h1>MentorWave</h1>
+        </div>
+        <div className="navbar-links">
+          <Menu.Item
+            name="Home"
+            active={activeItem === "Home"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name="About Us"
+            active={activeItem === "About Us"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name="Find Mentors"
+            active={activeItem === "Find Mentors"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name="My Account"
+            active={activeItem === "My Account"}
+            onClick={handleItemClick}
+          />
+        </div>
+        <div className="navbar-button">
+          <Menu.Menu position="right">
+            <Button
+              className="ui tiny button navbar-custom-button"
+              positive
+              animated="fade"
+            >
+              <Button.Content visible>Meet Your Mentor/Mentee</Button.Content>
+              <Button.Content hidden>Sign Up</Button.Content>
+            </Button>
+          </Menu.Menu>
+        </div>
+      </Menu>
+    </Segment>
   );
 };
 
-export default NavBar;
+export default Navbar;
