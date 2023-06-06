2ae48ec6003290d9c97aa7b76c90d6c1094ef26a
Kelvin Kimaru <kelvinkimaru@gmail.com>
2023-06-06 05:15:20 +0300

    Login and registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
 import "./App.css";
-import Mentors from "./components/mentorspage/Mentors";
-
+import react, { useState } from "react";
+// import Mentors from "./components/mentorspage/Mentors";
+import Login from "./Components/LoginSection/Login";
+import Registration from "./Components/LoginSection/Registration";
+// import userdata from "./data/user"
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState("");
+
+  //function to handle logout
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setUser(null);
+  };
+
+  //function to handle login
+  const handleLogin = (user) => {
+    setLoggedIn(true);
+    setUser(user);
+  };
+
+  //function to handle registration
+  const handleRegistration = (user) => {
+    setLoggedIn(true);
+    setUser(user);
+  };
+
   return (
-    <div>
+    <div className="App">
       <h1 className="ui centered header">Hello from MentorWave</h1>
-      <Mentors/>
+      {/* <Mentors/> */}
+      {loggedIn ? (
+        <div>
+          <h1>Welcome {user.username}</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <Login handleLogin={handleLogin} />
+          <Registration handleRegistration={handleRegistration} />
+        </div>
+      )}
     </div>
   );
 }
