2be1d377f7bbf43ea4a19100b4a6398300d072df
donvine mugendi <donvinemugendi@gmail.com>
2023-06-07 13:00:47 +0300

    updated mentorscard

--- src/components/home/MentorCard.jsx
+++ src/components/home/MentorCard.jsx
@@ -1,23 +1,6 @@
 import React, { useState } from "react";
 import "./Homecards.css";
 
-function MentorPicture({ image }) {
-  return (
-    < >
-      <img src={image} alt="Profile" className="card-image" />
-    </>
-  );
-}
-
-function MentorInfo({ name, profession }) {
-  return (
-    <div className="card-name" >
-      <h3>{name}</h3>
-      <p>{profession}</p>
-    </div>
-  );
-}
-
 function MentorCard({ name, image, profession }) {
   const [isHovered, setIsHovered] = useState(false);
 
@@ -27,12 +10,21 @@ function MentorCard({ name, image, profession }) {
 
   return (
     <div className="card" onMouseEnter={handleHover} onMouseLeave={handleHover}>
-      <div>
-        <MentorPicture image={image} />
-        {isHovered && <MentorInfo name={name} profession={profession} />}
-      </div>
+      {isHovered ? (
+        <div className="ui card">
+          <img src={image} alt="Profile" className="card-image" />
+          <div className="card-name">
+            <h3>{name}</h3>
+            <p>{profession}</p>
+          </div>
+        </div>
+      ) : (
+        <img src={image} alt="Profile" className="card-image" />
+      )}
     </div>
   );
 }
 
 export default MentorCard;
+
+
