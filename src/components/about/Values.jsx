0e790454ab420592e96e308546be55f49e1c2cd4
Wilder Ondieki <126570212+wondieki@users.noreply.github.com>
2023-06-08 16:22:46 +0300

    Update Values.jsx

--- src/components/about/Values.jsx
+++ src/components/about/Values.jsx
@@ -9,11 +9,11 @@ function Values() {
       <li>
     <strong>Excellence:</strong>Striving for excellence in mentorship, providing exceptional guidance, and consistently delivering high-quality support to mentees. MentorHub values excellence in every aspect of its operations, aiming to exceed expectations and drive meaningful results.</li>
     <li>
-      <strong>Excellence:</strong>Fostering a culture of empathy and understanding towards the needs, challenges, and aspirations of mentees. MentorHub recognizes the importance of empathy in building strong mentor-mentee relationships and creating a supportive environment for growth and development.</li>
+      <strong>Empathy:</strong>Fostering a culture of empathy and understanding towards the needs, challenges, and aspirations of mentees. MentorHub recognizes the importance of empathy in building strong mentor-mentee relationships and creating a supportive environment for growth and development.</li>
       <li>
         <strong>Collaboration:</strong>Promoting collaboration among mentors, mentees, and stakeholders to foster a sense of community and facilitate knowledge sharing. MentorHub encourages an inclusive and collaborative approach, recognizing that collective efforts and diverse perspectives lead to greater innovation and success.</li>
         <li>
-          <strong>Collaboration:</strong>Striving to make a positive and meaningful impact on the lives of mentees, companies, and the broader community. MentorHub is driven by the desire to empower individuals, drive growth, and contribute to positive change in the professional and educational landscape.</li>
+          <strong>Impact:</strong>Striving to make a positive and meaningful impact on the lives of mentees, companies, and the broader community. MentorHub is driven by the desire to empower individuals, drive growth, and contribute to positive change in the professional and educational landscape.</li>
       </ul>
     </div>
   );
