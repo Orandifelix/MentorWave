3cd44aa155aec85f830cddfa4e950ad64c16fa8d
Kelvin Kimaru <kelvinkimaru@gmail.com>
2023-06-08 15:25:11 +0300

    registration resolved

--- src/components/login/Registration.jsx
+++ src/components/login/Registration.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+// import { useState } from "react";
 // import './Registration.css'
 
 
-function Registration({ handleRegister, handleLoginForm }) {
-  const [userData, setUserData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    role: "",
-    agreedToTerms: false,
-  });
+// eslint-disable-next-line react/prop-types
+function Registration({ handleRegister, handleLoginForm, userData, setUserData }) {
+
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -22,19 +16,12 @@ function Registration({ handleRegister, handleLoginForm }) {
     console.log(userData);
   };
 
-  //handle checkbox change
-  const handleCheckboxChange = (event) => {
-    const { checked } = event.target;
-    setUserData((prevData) => ({
-      ...prevData,
-      agreedToTerms: checked,
-    }));
-  };
+
 
   //handle submit
   const handleSubmit = (event) => {
     event.preventDefault();
-    handleRegister()
+    handleRegister();
     setUserData({
       firstName: "",
       lastName: "",
@@ -50,54 +37,54 @@ function Registration({ handleRegister, handleLoginForm }) {
       className="ui padded container"
       style={{ margin: "20px auto", width: "50%" }}>
       <h2 className="ui padded header centered">Sign Up</h2>
-      <div class="ui attached message">
+      <div className="ui attached message">
         <div><h1>Welcome to MentorWave</h1></div>
         <h4>Fill out the form below to sign-up for a new account</h4>
       </div>
-      <form class="ui form attached fluid segment" onSubmit={handleSubmit}>
-        <div class="two fields">
-          <div class="field">
+      <form className="ui form attached fluid segment" onSubmit={handleSubmit}>
+        <div className="two fields">
+          <div className="field">
             <label>First Name</label>
             <input placeholder="First Name" name="firstName" type="text" value={userData.firstName} onChange={handleChange} required/>
           </div>
-          <div class="field">
+          <div className="field">
             <label>Last Name</label>
             <input placeholder="Last Name" type="text"name="lastName" value={userData.lastName} onChange={handleChange} required />
           </div>
         </div>
-        <div class="field">
+        <div className="field">
           <label>Username</label>
           <input placeholder="Username" type="text" required />
         </div>
-        <div class="field">
+        <div className="field">
           <label>Email</label>
           <input placeholder="Username" type="email" name="email" value={userData.email} onChange={handleChange} required />
         </div>
-        <div class="field">
+        <div className="field">
           <label>Password</label>
           <input type="password" name="password" minLength={8} value={userData.password} onChange={handleChange}/>
         </div>
-        <div class="field">
+        <div className="field">
           <label>Confirm Password</label>
           <input type="password" name="confirmPassword" value={userData.confirmPassword} onChange={handleChange} />
         </div>
-        <div class="field">
-        <select name="Role" value={userData.role} onChange={handleChange}>
+        <div className="field">
+        <select name="role" value={userData.role} onChange={handleChange}>
           <option value="">Role</option>
           <option value="1">Mentee</option>
           <option value="0">Mentor</option>
         </select>
         </div>
-        <div class="inline field">
-          <div class="ui checkbox">
-            <input type="checkbox" id="terms" tabindex="0" class="hidden" value={userData.agreedToTerms} onChange={handleChange} />
-            <label for="terms">I agree to the terms and conditions</label>
+        <div className="inline field">
+          <div className="ui checkbox">
+            <input type="checkbox" id="terms" className="hidden" value={userData.agreedToTerms} onChange={handleChange} />
+            <label htmlFor="terms">I agree to the terms and conditions</label>
           </div>
         </div>
-        <button class="ui blue submit button" onClick={handleLoginForm}>Register</button>
+        <button className="ui blue submit button" onClick={handleLoginForm}>Register</button>
       </form>
-      <div class="ui bottom attached warning message">
-        <i class="icon large help"></i>
+      <div className="ui bottom attached warning message">
+        <i className="icon large help"></i>
         Already signed up ? <a href="#">Login here</a> instead.
       </div>
     </div>
