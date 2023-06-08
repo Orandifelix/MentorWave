f3f3692a237e96f13b78b951785138021bc0d373
donvine mugendi <donvinemugendi@gmail.com>
2023-06-08 12:16:19 +0300

    updated about section

--- src/components/about/Testimonials.jsx
+++ src/components/about/Testimonials.jsx
@@ -1,36 +1,66 @@
-import React from 'react';
-import './aboutus.css'; 
+import React from "react";
+import "./aboutus.css";
 
 const Testimonials = () => {
   return (
-    <div className="testimonials-container">
-      <h2>What Our Mentees Say</h2>
-      <div className="testimonial-card-1">
-        <p className="testimonial-text">
-          "I had the privilege of being mentored by an incredible professional through MentorWave.Thanks to their mentorship, I gained confidence, expanded my network, and achieved my professional goals. I highly recommend MentorWave to anyone seeking mentorship opportunities."
-        </p>
-        <p className="testimonial-author">- Sarah, mentee</p>
+    <>
+      <h1 className="ui centered header">What Our Mentees Say</h1>
+      <div className="ui four doubling stackable padded cards segment">
+        <div className="ui card">
+          <blockquote className="card content">
+          <i className="quote left large blue icon centered"></i>
+            I had the privilege of being mentored by an incredible professional
+            through MentorWave.Thanks to their mentorship, I gained confidence,
+            expanded my network, and achieved my professional goals. I highly
+            recommend MentorWave to anyone seeking mentorship opportunities.
+            <i className="quote right blue icon centered"></i>
+          </blockquote>
+          <h3 className="card content header">
+            - Sarah,<span className="ui italized header">mentee</span>{" "}
+          </h3>
+        </div>
+        <div className="ui card">
+          <blockquote className="card content">
+          <i className="quote left icon large red centered"></i>
+            MentorWave has been a game-changer for me.Our mentoring sessions
+            were transformative. My mentor went above and beyond to provide
+            personalized guidance, offer constructive feedback, and share
+            invaluable resources. I am grateful for Mentor Hub's commitment to
+            fostering meaningful mentor-mentee relationships. <i className="quote right red icon centered"></i>
+          </blockquote>
+          <h3 className="card content header">
+            - John, <span className="ui italized header">mentee</span>
+          </h3>
+        </div>
+        <div className="ui card">
+          <blockquote className="card content">
+            <i className="quote left green large icon centered"></i>
+            MentorWave exceeded my expectations in every way. As a recent
+            graduate entering a competitive job market, I was unsure of my
+            career direction.Their mentorship gave me the clarity and motivation
+            I needed to succeed. <i className="quote right green icon centered"></i>
+          </blockquote>
+          <h3 className="card content header">
+            - Emily, <span className="ui italized header">mentee</span>
+          </h3>
+        </div>
+        <div className="ui card">
+          <blockquote className="card content">
+            <i className="quote left icon pink large centered"></i>Being part of MentorWave
+            has been an incredible experience. The platform not only connected
+            me with a highly knowledgeable and accomplished mentor but also
+            provided a structured framework for our mentorship journey. My
+            mentor helped me set meaningful goals, develop new skills, and
+            overcome challenges. Their unwavering support and commitment to my
+            growth were truly remarkable.{" "}
+            <i className="quote right pink icon centered"></i>
+          </blockquote>
+          <h3 className="card content header">
+            - Michael, <span className="ui italized header">mentee</span>
+          </h3>
+        </div>
       </div>
-      <div className="testimonial-card-2">
-        <p className="testimonial-text">
-          "MentorWave has been a game-changer for me.Our mentoring sessions were transformative. My mentor went above and beyond to provide personalized guidance, offer constructive feedback, and share invaluable resources. I am grateful for Mentor Hub's commitment to fostering meaningful mentor-mentee relationships."
-        </p>
-        <p className="testimonial-author">- John, mentee</p>
-      </div>
-      <div className="testimonial-card-3">
-        <p className="testimonial-text">
-          "MentorWave exceeded my expectations in every way. As a recent graduate entering a competitive job market, I was unsure of my career direction.Their mentorship gave me the clarity and motivation I needed to succeed."
-        </p>
-        <p className="testimonial-author">- Emily, mentee</p>
-      </div>
-      <div className="testimonial-card-4">
-        <p className="testimonial-text">
-          "Being part of MentorWave has been an incredible experience. The platform not only connected me with a highly knowledgeable and accomplished mentor but also provided a structured framework for our mentorship journey. My mentor helped me set meaningful goals, develop new skills, and overcome challenges. Their unwavering support and commitment to my growth were truly remarkable."
-        </p>
-        <p className="testimonial-author">- Michael, mentee</p>
-      </div>
-      
-    </div>
+    </>
   );
 };
 
--- src/components/about/Vision.jsx
+++ src/components/about/Vision.jsx
@@ -3,14 +3,14 @@ import './aboutus.css';
 
 function Vision() {
   return (
-    <div className="vision-container">
-      <h2>OUR VISION</h2>
-      <p className="vision-description">
+    <div className="ui padded raised segment">
+      <h2 className='ui centered header'>Our Vision</h2>
+      <h4 className="ui content">
       Our vision is to be the leading mentorship platform that transforms the way companies and students approach learning and professional development. We envision a future where mentorship is deeply ingrained in the fabric of organizations and educational institutions, enabling individuals to thrive, innovate, and achieve their highest aspirations.
-      </p>
-      <p className="vision-description1">
-      Key elements of our vision:Transforming learning and development,empowering organizations,nurturing future leaders,fostering collaboration and innovation & making mentorship accessible
-    </p>
+      </h4>
+      <h4 className="ui content">
+      Key elements of our vision: Transforming learning and development,empowering organizations,nurturing future leaders,fostering collaboration and innovation & making mentorship accessible
+    </h4>
     </div>
   );
 }
