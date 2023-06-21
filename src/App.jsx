ca53a8e93bc0295fe411a02fd15a33a6f2e5b6c0
donvine mugendi <donvinemugendi@gmail.com>
2023-06-21 21:33:57 +0300

    updated lazy loading of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
 import NavBar from "./components/home/NavBar";
 import Footer from "./components/home/Footer";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Mentors from "./components/mentorspage/Mentors";
-import { useState } from "react";
+import React, { useState } from "react";
 import LoginRegistration from "./components/login/LoginRegistration";
 import Home from "./components/home/Home";
-import About from "./components/about/About";
-import MyAccount from "./components/account/MyAccount";
-import Blog from "./components/blog/Blog";
+const LazyABout = React.lazy(() => import("./components/about/About"));
+const LazyBlog = React.lazy(() => import("./components/blog/Blog"));
+const LazyMyAccount = React.lazy(() =>
+  import("./components/account/MyAccount")
+);
+const LazyMentors = React.lazy(() =>
+  import("./components/mentorspage/Mentors")
+);
 
 function App() {
   const [login, setLogin] = useState(false);
@@ -22,24 +25,74 @@ function App() {
     navigate("/");
   };
   if (login) {
-    return <LoginRegistration handleLoginForm={handleLoginForm}/>;
+    return <LoginRegistration handleLoginForm={handleLoginForm} />;
   }
   return (
-
     <>
       <NavBar handleClick={handleLoginClick} />
-      <hr/>
+      <hr />
       <Routes>
         <Route path="/" element={<Home handleClick={handleLoginClick} />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/mentors" element={<Mentors handleLoginClick={handleLoginClick} />} />
-        <Route path="/account" element={<MyAccount />} />
+        <Route
+          path="/about"
+          element={
+            <React.Suspense
+              fallback={
+                <div className="ui segment">
+                  <div className="ui active loader"></div>
+                  <h2>Hang in there..</h2>
+                </div>
+              }>
+              <LazyABout />{" "}
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/blog"
+          element={
+            <React.Suspense
+              fallback={
+                <div className="ui segment">
+                  <div className="ui active loader"></div>
+                  <h2>Hang in there..</h2>
+                </div>
+              }>
+              <LazyBlog />{" "}
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/mentors"
+          element={
+            <React.Suspense
+              fallback={
+                <div className="ui segment">
+                  <div className="ui active loader"></div>
+                  <h2>Hang in there..</h2>
+                </div>
+              }>
+              <LazyMentors handleLoginClick={handleLoginClick} />{" "}
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/account"
+          element={
+            <React.Suspense
+              fallback={
+                <div className="ui segment"Name>
+                  <div className="ui active loader"></div>
+                  <h2>Hang in there..</h2>
+                </div>
+              }>
+              <LazyMyAccount />{" "}
+            </React.Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </>
   );
 }
-
 
 export default App;
