c8d217f221f4134be3db959bcba384a3c6c628eb
donvine mugendi <donvinemugendi@gmail.com>
2023-06-23 05:41:34 +0300

    Only god understands this code

--- src/components/about/TeamMembers.jsx
+++ src/components/about/TeamMembers.jsx
@@ -5,14 +5,14 @@ import { teamMembers } from '../../data';
 function TeamMembers() {
 
   return (
-    <div className='ui raised segment'>
+    <div className='ui raised segment' style={{backgroundColor:"#f7f3fe"}}>
       <h2 className='ui centered header'>Our Team</h2>
-    <div className="ui four doubling stackable padded cards segment">
+    <div className="ui four doubling stackable padded cards segment"style={{backgroundColor:"#f7f3fe"}}>
         {teamMembers.map((member, index) => (
-          <div key={index} className="ui card">
+          <div key={index} className="ui card" style={{backgroundColor:"#f7f3fe"}}>
             <div className='card content'>
               <div className='ui flex container custom-flex-container'>
-            <img src={member.image} className='ui circular image' id='custom-image'/>
+            <img src={member.image} className='ui medium circular image' id='custom-image'/>
             <h2 className='custom-h2'>{member.name}</h2>
             <h4>{member.role}</h4>
             </div>
